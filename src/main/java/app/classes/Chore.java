ccb254313417b5202948de3f946057fa459efcc0
wikror <wiktor.rorot@gmail.com>
2017-06-24 00:18:56 +0200

    basic backend classes

--- src/main/java/app/classes/Chore.java
+++ src/main/java/app/classes/Chore.java
@@ -1,7 +1,15 @@
 package app.classes;
 
+import java.util.Date;
+
 /**
  * Created by wikto on 23.06.2017.
  */
 public class Chore {
+    private Inmate inmate;
+    private Date date;
+    private String toString;
+
+    public Chore(Inmate inmate, Date date) {
+    }
 }
---
+++ src/main/java/app/classes/ChoresTypes.java
@@ -0,0 +1,11 @@
+package app.classes;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by wikto on 24.06.2017.
+ */
+public enum ChoresTypes {
+    //Lists all classes that extend the Chore abstract class
+    TRASH, VACUUMING, SHOPPING, CLEANINGBATHROOM, OTHER;
+}
